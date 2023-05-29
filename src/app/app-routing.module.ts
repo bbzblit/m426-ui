27e688bd0d8a28be6d30f03d15be2a9ee6c78096
Yannick Müller <mail@ynnk.dev>
2023-05-29 20:05:40 +0200

    add angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarTableComponent } from './pages/car-table/car-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "car", pathMatch: "full", component: CarTableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
