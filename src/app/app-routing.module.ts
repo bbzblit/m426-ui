78ab2c69d4c7939914d320d3a65a78865a9b6a6f
leon005 <leon.raetzo@bbzbl-it.ch.com>
2023-05-31 08:22:55 +0200

    add sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarTableComponent } from './pages/car-table/car-table.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SingUpComponent } from './pages/sing-up/sing-up.component';
 
 const routes: Routes = [
   {path: "car", pathMatch: "full", component: CarTableComponent },
   {path: "login", pathMatch: "full", component: LoginComponent},
+  {path: "sign-up", pathMatch: "full", component: SingUpComponent},
 ];
 
 @NgModule({
