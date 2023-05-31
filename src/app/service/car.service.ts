8e5a584f88b68884c8ecf66a0ba6dc0d4bc6b0af
Yannick Müller <mail@ynnk.dev>
2023-05-31 09:09:13 +0200

    add dialog methods

--- src/app/service/car.service.ts
+++ src/app/service/car.service.ts
@@ -3,7 +3,7 @@ import { Car } from '../dataaccess/car';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { identifierName } from '@angular/compiler';
+import { identifierName, ReturnStatement } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -28,5 +28,9 @@ export class CarService {
 
   public updateCar(car: Car): Observable<Car>{
     return this.http.put<Car>("/api/car/" + car.id, car);
+  }
+
+  public deleteCar(id: number): Observable<void>{
+    return this.http.delete<void>("/api/car/" + id);
   }
 }
