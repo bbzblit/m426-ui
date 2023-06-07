c50a950d9ff3c2e90ee2b90ed7fbe4144b2a4812
Yannick Müller <mail@ynnk.dev>
2023-06-07 21:12:13 +0200

    fixed reservation list

--- src/app/pages/reservation-list/reservation-list.component.ts
+++ src/app/pages/reservation-list/reservation-list.component.ts
@@ -7,11 +7,14 @@ import { ReservationService } from 'src/app/service/reservation.service';
   templateUrl: './reservation-list.component.html',
   styleUrls: ['./reservation-list.component.less']
 })
-export class ReservationListComponent implements OnInit{
+export class ReservationListComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'car', 'user', 'start', 'end'];
 
   public reservations: Array<Reservation> = [];
+  public previewReservations: Array<Reservation> = [];
+  public index = 0;
+  public pageSize = 10;
 
   constructor(private reservationService: ReservationService) {
 
@@ -19,8 +22,24 @@ export class ReservationListComponent implements OnInit{
 
   ngOnInit(): void {
     this.reservationService.getReservations().subscribe({
-      next: (reservations) => this.reservations = reservations
+      next: (reservations) => { this.reservations = reservations; this.updatePreviewReservations() }
     })
+  }
+
+  updatePreviewReservations() {
+    const length = this.reservations.length;
+    if (this.pageSize * (this.index + 1) > length) {
+      this.previewReservations = this.reservations.slice(this.pageSize * this.index, length);
+    } else {
+      this.previewReservations = this.reservations.slice(this.pageSize * this.index, this.pageSize * (this.index + 1));
+    }
+  }
+
+
+  changePage(event: any) {
+    this.index = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.updatePreviewReservations();
   }
 
 }
