f05941b3d5bce3080c55f477c7547844cb1a7615
Yannick Müller <mail@ynnk.dev>
2023-06-07 21:02:01 +0200

    fixed car list view

--- src/app/pages/car-table/car-table.component.ts
+++ src/app/pages/car-table/car-table.component.ts
@@ -11,22 +11,46 @@ import { CarService } from 'src/app/service/car.service';
   styleUrls: ['./car-table.component.less']
 })
 export class CarTableComponent {
-  displayedColumns: string[] = ['id', 'serialnumber', 'licenceplate', 'brand', "model" ,'color', 'action'];
+
+  public displayedColumns: string[] = ['id', 'serialnumber', 'licenceplate', 'brand', "model", 'color', 'action'];
+  public previewCars: Array<Car> = [];
+  public cars: Array<Car> = [];
+  public index = 0;
+  public pageSize = 10;
+
   CarDataSource = new MatTableDataSource<Car>();
 
-  constructor(private carService: CarService, private router: Router, private _snakBar: MatSnackBar) {}
 
-  async ngOnInit() {
-    await this.reloadData();
+  constructor(private carService: CarService, private router: Router, private _snakBar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.reloadData();
   }
+
+  updatePreviewCars() {
+    console.log(this.cars)
+    const length = this.cars.length;
+    if (this.pageSize * (this.index + 1) > length) {
+      this.previewCars = this.cars.slice(this.pageSize * this.index, length);
+    } else {
+      this.previewCars = this.cars.slice(this.pageSize * this.index, this.pageSize * (this.index + 1));
+    }
+  }
+
+
+  changePage(event: any) {
+    this.index = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.updatePreviewCars();
+  }
+
+
 
   reloadData() {
-    this.carService.getList().subscribe(obj => {
-      this.CarDataSource.data = obj;
-    });
+    this.carService.getList().subscribe(cars => { this.cars = cars; this.updatePreviewCars() });
   }
 
-  deleteCar(id: number){
+  deleteCar(id: number) {
     this.carService.deleteCar(id).subscribe({
       next: () => this.reloadData(),
       error: () => this._snakBar.open("Car got already reservations you need to cancel this first", "Okay"),
