31a3a0855c42b612b2fb8f3ed202c3a0cb5df58d
Yannick Müller <mail@ynnk.dev>
2023-06-27 19:45:13 +0200

    menu loads generic to roles

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/dataaccess/user.model';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.less']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  
+  public user: User | null = null;
+
+  constructor(private userservice: UserService){}
+
+  ngOnInit(): void {
+    this.userservice.whoami().subscribe(user => this.user = user);
+  }
 
 }
